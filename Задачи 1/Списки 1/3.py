4aa95d1c21bef907640b0cbb5b04809205bff442
nikitosroslik <147470643+nikitosroslik@users.noreply.github.com>
2023-12-24 22:22:46 +0500

    Create 3.py

---
+++ Задачи 1/Списки 1/3.py
@@ -0,0 +1,5 @@
+from collections import Counter
+def top3(st):
+    counter_top3 = Counter(st.replace(' ', '')).most_common(3)
+    return ', '.join([f'{letter} - {cnt}' for letter, cnt in counter_top3])
+print(top3('Макс вышел из дома и пошел в школу'))
