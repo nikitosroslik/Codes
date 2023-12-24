61107360d96fd52b392137f95bccc833efb89c36
nikitosroslik <147470643+nikitosroslik@users.noreply.github.com>
2023-12-24 23:29:18 +0500

    Create 3.py

---
+++ Задачи 3/3.py
@@ -0,0 +1,23 @@
+deck = [6,7,8,9,10,2,3,4,11] * 4
+import random
+random.shuffle(deck)
+print('Добро пожаловать!')
+count = 0
+while True:
+    choice = input('Будете брать карту? да/нет\n')
+    if choice == 'да':
+        current = deck.pop()
+        print('Вам попалась карта достоинством %d' %current)
+        count += current
+        if count > 21:
+            print('Вы проиграли')
+            break
+        elif count == 21:
+            print('Поздравляю, вы набрали 21!')
+            break
+        else:
+            print('У вас %d очков.' %count)
+    elif choice == 'нет':
+        print('У вас %d очков и вы закончили игру.' %count)
+        break
+print('ББ')
