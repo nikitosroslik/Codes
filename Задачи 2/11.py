2b0bcf5b5058cf0c25d3af12bacd0a9f27c93031
nikitosroslik <147470643+nikitosroslik@users.noreply.github.com>
2023-12-24 22:45:22 +0500

    Update 11.py

--- Задачи 2/11.py
+++ Задачи 2/11.py
@@ -1,5 +1,5 @@
 array = ['1 гость', '2 гость', '3 гость']
 def zdarova(array):
     for i in range(len(array)):
-        print(f'Здравствуй!, {array[i]}')
+        print(f'Здравствуй!', {array[i]}')
 zdarova(array)
